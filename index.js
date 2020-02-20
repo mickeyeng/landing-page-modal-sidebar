10e84154a7d761ca6e83c32d09d965ab0f3c51c4
mickey english <mickeyeng92@gmail.com>
2020-02-20 11:22:18 +0000

    show modal working

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 console.log('working!');
 
 const sidebarToggle = document.getElementById('toggle');
-const sidebarClose = document.getElementById('close');
-const sidebarOpen = document.getElementById('open');
+const modalClose = document.getElementById('close');
+const modalOpen = document.getElementById('open');
 const modal = document.getElementById('modal');
 
 // Toggle nav
-
 sidebarToggle.addEventListener('click', () => {
   console.log('clicked!');
   document.body.classList.toggle('show-nav');
+});
+
+// show modal
+modalOpen.addEventListener('click', () => {
+  console.log('clicked');
+  modal.classList.add('show-modal');
+});
+
+//close modal
+modalClose.addEventListener('click', () => {
+  modal.classList.remove('show-modal');
+});
+
+// Hide modal on outside click
+window.addEventListener('click', e => {
+  e.target === modal ? modal.classList.remove('show-modal') : false;
 });
